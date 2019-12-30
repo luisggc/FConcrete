e87c00048e6cf5f70d929ec1177d99749f124de6
Luis Coimbra <luiscoimbraeng@outlook.com>
2019-12-29 23:30:32 -0300

    table in AvailableTransvConcreteSteelBar

--- fconcrete/AvailableMaterials.py
+++ fconcrete/AvailableMaterials.py
@@ -63,7 +63,7 @@ class AvailableTransvConcreteSteelBar:
                     },
                  space_is_multiple_of=[5],
                  fyw = 50,
-                 max_number=100):
+                 max_number=3):
         try:
             areas = [diameters_to_area[diameter] for diameter in diameters]
         except:
@@ -71,30 +71,31 @@ class AvailableTransvConcreteSteelBar:
         
         possible_spaces = np.array([])
         for multiple in space_is_multiple_of:
-            possible_spaces = np.concatenate((possible_spaces,multiple*np.arange(1,50)))
-        self.possible_spaces = np.unique(possible_spaces)
+            possible_spaces = np.concatenate((possible_spaces,multiple*np.arange(1,30)))
+        possible_spaces = np.unique(possible_spaces[possible_spaces <= 30])
+        
+        
+        areas = [ diameters_to_area[diameter] for diameter in diameters ]
+        diameters_loop = np.tile(diameters, len(possible_spaces))
+        spaces_loop = np.repeat(possible_spaces, len(diameters))
+        areas_loop = 2*np.tile(areas, len(possible_spaces))
+
+        table = np.transpose([
+            diameters_loop,
+            spaces_loop,
+            areas_loop,
+            areas_loop/spaces_loop
+        ])
 
         fyd = fyw/1.15
         diameters = np.array(diameters)
-        areas = np.array(areas)
-        diameters_loop = np.tile(diameters/10, max_number-1)
-        # Single steel bar use not allowed, that is why range starts at 2
-        areas_loop = np.concatenate([ areas*(i) for i in range(2, max_number+1)])
-        number_of_bars = areas_loop/np.tile(areas,max_number-1)
-        table_of_positive_steel = np.stack((number_of_bars, diameters_loop, areas_loop), axis=1)
         
-        table_of_negative_steel = np.stack((number_of_bars, -diameters_loop, -areas_loop), axis=1)
-        table_of_positive_and_negative_steel = np.vstack([table_of_negative_steel, table_of_positive_steel])
-        
-        table = np.array(table_of_positive_and_negative_steel[
-            table_of_positive_and_negative_steel[:,2].argsort()
-        ])
-        self.table = table
         self.fyw = fyw
         self.fyd = fyd
+        self.table = table[table[:,3].argsort()]
+        
         self.diameters = diameters
         self.diameters_to_area = diameters_to_area
-    
     
     
         
