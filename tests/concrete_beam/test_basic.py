3f72adc2acff696c80995b0f8f8f1d0c06f9dc12
Luis Coimbra <luiscoimbraeng@outlook.com>
2020-01-01 17:25:08 -0300

    Test Structure Adaptation

--- tests/concrete_beam/test_basic.py
+++ tests/concrete_beam/test_basic.py
@@ -12,14 +12,15 @@ def create_concrete_beam():
     material = fc.Concrete(fck='30 MPa', aggressiveness=2)
     section = fc.Rectangle(25,56, material)
 
+    #Design
     f1 = fc.Load.UniformDistributedLoad(-0.1622, x_begin=0, x_end=113)
     f2 = fc.Load.UniformDistributedLoad(-0.4994, x_begin=113, x_end=583)
     f3 = fc.Load.UniformDistributedLoad(-0.4196, x_begin=583, x_end=1188)
 
-    n1 = fc.Node.SimpleSupport(x=0)
-    n2 = fc.Node.SimpleSupport(x=113)
-    n3 = fc.Node.SimpleSupport(x=583)
-    n4 = fc.Node.SimpleSupport(x=1188)
+    n1 = fc.Node.SimpleSupport(x=0, length=20)
+    n2 = fc.Node.SimpleSupport(x=113, length=20)
+    n3 = fc.Node.SimpleSupport(x=583, length=20)
+    n4 = fc.Node.SimpleSupport(x=1188, length=20)
 
     bar1 = fc.SingleBeamElement([n1, n2], section)
     bar2 = fc.SingleBeamElement([n2, n3], section)
@@ -27,15 +28,14 @@ def create_concrete_beam():
 
     fc.config.available_material = {
         "concrete_long_steel_bars":fc.AvailableLongConcreteSteelBar(diameters=[8]),
-        "steel_bar_surface_type":"ribbed"
+        "concrete_transv_steel_bars":fc.AvailableTransvConcreteSteelBar(diameters=[8]),
     }
         
     beam = fc.ConcreteBeam(
         loads = [f1, f2, f3],
         beam_elements = [bar1, bar2, bar3],
-        #bar_steel_max_removal = 2,
+        bar_steel_max_removal = 2,
     )
-    
     return beam
     
 def test_create_concrete_beam():
